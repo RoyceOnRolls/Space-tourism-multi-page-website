cecd9aa383bc175f62e36930eb1b5565331c0012
royceonrolls <oroycerolls@gmail.com>
2022-09-14 22:40:17 +0100

    Added technology page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import Home from "./Pages/Home/Home";
 import Destination from "./Pages/Destinations/Destinations";
 import Crew from "./Pages/Crew/Crew";
+import Technology from "./Pages/Technology/Technology";
 
 function App() {
   return (
     <div>
-      <Crew />
+      <Technology />
     </div>
   );
 }
